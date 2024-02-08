4115dbc0c2b92914349efc4680499dad175f892f
Student <ja.borkowski@aulanz.net>
2024-02-08 17:27:22 +0100

    ok

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ print("Hola")
 i = 1
 while i ==True:
     print("Nooooooooo")
+    i = 0
 
-    
+
 while True:
     print("Stary, wszyscy jesteście głupi, nie chcę być obraźliwy, ale tak po prostu jest.")
+    False
